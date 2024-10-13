01c6c9ef5a775ecbe01a60838c1bead387221614
Akash <akabhowmick@gmail.com>
2024-10-13 14:51:04 -0400

    remove contact box

--- src/components/Shared/ContactInfoBox.tsx
+++ src/components/Shared/ContactInfoBox.tsx
@@ -1,15 +1,15 @@
-import { FiPhone, FiMail, FiMapPin } from "react-icons/fi";
+// import { FiPhone, FiMail, FiMapPin } from "react-icons/fi";
 import { CareerForm } from "../Pages/CareersPage/CareerForm";
 import { ContactForm } from "../Pages/ContactPage/ContactForm";
 
 export const ContactInfoBox = ({ formType }: { formType: string }) => {
-  const cardStyles = "overflow-hidden bg-white rounded-xl";
-  const cardContentStyles = "p-6 text-lg";
-  const iconStyles = "flex-shrink-0 w-10 h-10 mx-auto text-gray-400";
+  // const cardStyles = "overflow-hidden bg-white rounded-xl";
+  // const cardContentStyles = "p-6 text-lg";
+  // const iconStyles = "flex-shrink-0 w-10 h-10 mx-auto text-gray-400";
 
   return (
     <div className="max-w-5xl mx-auto my-12 sm:mt-16">
-      <div className="grid grid-cols-1 gap-6 px-2 text-center md:px-0 md:grid-cols-3">
+      {/* <div className="grid grid-cols-1 gap-6 px-2 text-center md:px-0 md:grid-cols-3">
         <div className={cardStyles}>
           <div className={cardContentStyles}>
             <FiPhone className={iconStyles} />
@@ -58,7 +58,7 @@ export const ContactInfoBox = ({ formType }: { formType: string }) => {
             </p>
           </div>
         </div>
-      </div>
+      </div> */}
 
       {formType === "career" ? <CareerForm /> : <ContactForm />}
     </div>
