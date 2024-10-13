1369a9625fe0f14c41cfad58fbb007700679275f
Akash <akabhowmick@gmail.com>
2024-10-13 11:32:58 -0400

    add insurance image

--- src/components/Pages/Resources/InsuranceInfo.tsx
+++ src/components/Pages/Resources/InsuranceInfo.tsx
@@ -1,11 +1,12 @@
 import { pageBanners } from "../../../data/content";
 import { SubPageTitle } from "../../Shared/CommonSections";
 import { commonStyles } from "../../Shared/CommonStyles";
+import insuranceImage from "../../../assets/siteImages/home/insurance.png";
 
 export const InsurancePage: React.FC = () => {
   return (
     <div className={commonStyles.containerStyles}>
-      <SubPageTitle pageBannerInfo={pageBanners.insurance}/>
+      <SubPageTitle pageBannerInfo={pageBanners.insurance} />
       <div className="max-w-4xl mx-auto p-6">
         {/* Icon and description */}
         <div className="text-center mb-8">
@@ -28,7 +29,7 @@ export const InsurancePage: React.FC = () => {
             <img
               alt="Family sitting together and smiling"
               className="rounded-full mx-auto"
-              src="https://storage.googleapis.com/a1aa/image/IxCO0qdGTwogLx2f4ZE2myly422NQfrYEnLQUsUav4teoMFnA.jpg"
+              src={insuranceImage}
               width="300"
               height="200"
             />
